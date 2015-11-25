8ef3d656230aa9c73b6295a62ba1d91e381405aa
YamTai <YamTai.Ow_2014@SIT.SingaporeTech.edu.sg>
2015-11-25 19:38:54 +0800

    just left with beeper

--- main.c
+++ main.c
@@ -15,11 +15,6 @@
 
 extern u16 xyz[3];
 
-unsigned short temp;
-
-/*
- * main.c
- */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
@@ -36,13 +31,10 @@ int main(void) {
     P2SEL |= 0x08;	//	P2.3 alternate function, TA1 CCR2 CCI2A
 
     TA0CTL |= TASSEL_1 | ID_3 | TACLR;	// ACLK (32768Hz) | /8 | timerA clear, 4096Hz (2^16/4096), MAX 16 seconds
-    TA0CCTL0 = CCIE;	//	timerA0 compare/control register 1 interrupt enable
 
 	TA1CTL |= TASSEL_2 | MC_2 | TACLR;	//	SMCLK (?MHz) | Continuous mode | timer A clear
 	TA1CCTL2 |= CM_1 | CCIS_0 | SCS | CAP | CCIE;	//	rising edge | input select CCI2A | Synchronous | Capture mode | Interrupt enable
 
-
-	accel_start();
     while(1){
 
         _BIS_SR(LPM3_bits + GIE);	// low power mode to save more powar
@@ -55,7 +47,7 @@ int main(void) {
 __interrupt void Port_2(void){
 
 	u8 *str;
-
+	u16 temp;
 
 	//	mgLSB are mg values per LSB, mutiplied by 100
 
@@ -91,6 +83,11 @@ __interrupt void Port_2(void){
 		}else{
 			display_char(LCD_SEG_L2_4, (u8)'R', SEG_ON);
 		}
+
+		TA0CCTL0 &= ~CCIE;	//	timerA0 compare/control register 0 interrupt disable
+		TA0CCTL1 &= ~CCIE;	//	timerA0 compare/control register 1 interrupt disable
+
+		TA0CTL |= MC_0 | TACLR;	//	stop and clear TimerA0
 	}
 
 	P2IFG &= ~0x1F;	// P2.0 ~ P2.4 interrupt flag clear
@@ -99,26 +96,45 @@ __interrupt void Port_2(void){
 }
 
 #pragma vector=TIMER0_A0_VECTOR
-__interrupt void TIMER_A0_CCR0_ISR(void){
+__interrupt void TIMER_A5_CCR0_ISR(void){
+
+//	TA0CCTL0 &= ~CCIFG;	//	TA0CCR0 interrupt flag auto clears
 
 	accel_stop();
 
-	display_chars(LCD_SEG_L2_4_0, (u8 *)"------", SEG_ON);
-	display_symbol(LCD_SEG_L2_COL0, SEG_OFF);
-	TA0CCR0 = 0;
-	TA0CTL |= MC_0 | TACLR;	//	stop TimerA0 in up mode
-	TA0CCTL0 &= ~CCIFG;
+	display_chars(LCD_SEG_L2_4_0, (u8 *)"OOOOO", SEG_ON);
+
+    TA0CCTL0 &= ~CCIE;	//	timerA0 compare/control register 0 interrupt disable
+    TA0CCTL1 &= ~CCIE;	//	timerA0 compare/control register 1 interrupt disable
+
+	TA0CTL |= MC_0 | TACLR;	//	stop and clear TimerA0
+}
+
+
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void TIMER_A5_CCR1_ISR(void){
+
+	if ((TA0CCTL1 & CCIFG) == 1){	//	TA0CCR1 interrupt
+
+		accel_start();
+
+		display_chars(LCD_SEG_L2_4_0, (u8 *)"-----", SEG_ON);
+
+		TA0CCTL1 &= ~CCIFG;	//	clear TA0CCR1 interrupt flag
+	}
 }
 
 #pragma vector=TIMER1_A1_VECTOR
-__interrupt void TIMER_A1_CCR2_ISR(void){
+__interrupt void TIMER_A3_CCR2_ISR(void){
 
 	u8 *str;
 	u16 rand;
 
 	if ((TA1CCTL2 & CCIFG) == 1){	//	P2.3 interrupt (CCI2A)
 
-		accel_start();
+		while((P2IN & BUTTON_BACKLIGHT_PIN) == BUTTON_BACKLIGHT_PIN);
+
+		TA1CCTL2 &= ~CCIFG;	// clear TA1CCTL2 interrupt
 
 		rand = TA1CCR2;
 		rand ^= rand << 8;
@@ -129,14 +145,16 @@ __interrupt void TIMER_A1_CCR2_ISR(void){
 			rand |= 0x3000;
 		}
 
-		str = int_to_array(rand, 5, 0);
+		str = int_to_array((rand/4096), 5, 0);	//	approximate delay
 		display_chars(LCD_SEG_L2_4_0, (u8 *)str, SEG_ON);
+		clear_line(1);
+		display_symbol(LCD_SEG_L2_COL0, SEG_OFF);
 
-		TA1CCTL2 &= ~CCIFG;	// clear TA1CCTL2 interrupt
+	    TA0CCTL0 |= CCIE;	//	timerA0 compare/control register 0 interrupt enable
+	    TA0CCTL1 |= CCIE;	//	timerA0 compare/control register 1 interrupt enable
 
-		TA0CCR0 = rand;	//	set TA0CCR0 to 'random' value
-		TA0CTL |= MC_1;	//	start TimerA0 in up mode
-
+		TA0CCR0 = rand + 0x1000;	//	set TA0CCR0 to 'random' value + offset of .5 seconds(ACLK/8)
+		TA0CCR1 = rand;				//	set TA0CCR1 to 'random' value
+		TA0CTL |= MC_1;				//	start TimerA0 in up mode
 	}
-
 }
